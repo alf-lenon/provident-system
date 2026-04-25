bfd3f8e02cc717db83b1c0eb17e5268ce24b41b0
Alf <sample@example.com>
2026-04-25 12:04:08 +0800

    feat: fetch and display applications in dashboard using useEffect and backend API

--- provident-system/src/pages/Dashboard.tsx
+++ provident-system/src/pages/Dashboard.tsx
@@ -1,53 +1,42 @@
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+type Application = {
+	borrower: {
+		fullName: string;
+	};
+	loan: {
+		loanAmount: string;
+		loanType: string;
+	};
+};
 
 function Dashboard() {
+	const [applications, setApplications] = useState<Application[]>([]);
+
+	useEffect(() => {
+		const fetchApplications = async () => {
+			try {
+				const response = await fetch('http://localhost:5000/applications'); // Request data from back end
+				const data = await response.json(); // Data or response from back end
+				setApplications(data); // Updates the state or Replaces current applications with new data
+			} catch (error) {
+				console.error('error', error);
+			}
+		};
+		fetchApplications();
+	}, []);
 	return (
-		<main className='min-h-screen bg-gray-100 p-6'>
-			{/* Header */}
-			<header className='mb-6'>
-				<h1 className='text-2xl font-bold text-gray-800'>
-					Provident Loan System
-				</h1>
-				<p className='text-gray-600'>Version 1 - Internal Pre-Screening Tool</p>
-			</header>
+		<div>
+			<h1>Applications</h1>
 
-			{/* Button */}
-			<section className='mb-6'>
-				<Link
-					to='/new'
-					className='bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700'
-				>
-					+ New Application
-				</Link>
-			</section>
-
-			{/* Applications */}
-			<section>
-				<h2 className='text-lg font-semibold mb-4 text-gray-800'>
-					Applications
-				</h2>
-
-				<div className='space-y-4'>
-					<div className='bg-white p-4 rounded-lg shadow'>
-						<p className='font-bold'>Juan Dela Cruz</p>
-						<p className='text-sm text-gray-600'>Loan Type: Renewal</p>
-						<p className='text-green-600 font-semibold'>Ready for Processing</p>
-					</div>
-
-					<div className='bg-white p-4 rounded-lg shadow'>
-						<p className='font-bold'>Maria Santos</p>
-						<p className='text-sm text-gray-600'>Loan Type: New</p>
-						<p className='text-yellow-600 font-semibold'>Needs Correction</p>
-					</div>
-
-					<div className='bg-white p-4 rounded-lg shadow'>
-						<p className='font-bold'>Pedro Reyes</p>
-						<p className='text-sm text-gray-600'>Loan Type: Renewal</p>
-						<p className='text-red-600 font-semibold'>Rejected</p>
-					</div>
+			{applications.map((app, index) => (
+				<div key={index}>
+					<p>Name: {app.borrower.fullName}</p>
+					<p>Loan amount: {app.loan.loanAmount}</p>
+					<p>Loan type: {app.loan.loanType}</p>
 				</div>
-			</section>
-		</main>
+			))}
+		</div>
 	);
 }
 
